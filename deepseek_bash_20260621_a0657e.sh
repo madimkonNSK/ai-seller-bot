bfe5e6eae0d4761bcf990c5526335b10cc3ed0c0
madimkonNSK <madimkon@gmail.com>
2026-06-21 18:30:47 +0300

    Add files via upload

---
+++ deepseek_bash_20260621_a0657e.sh
@@ -0,0 +1,6 @@
+# Создадим виртуальное окружение и активируем
+python -m venv venv
+source venv/bin/activate
+
+# Установим нужные библиотеки
+pip install aiogram chromadb httpx python-dotenv loguru
